259f24a0a7a18b5f3aeee350863ba8a26fd478f1
mohamedaliarafat <112913473+mohamedaliarafat@users.noreply.github.com>
2025-11-07 22:19:18 +0300

    Refactor Restaurant schema and fix verification field

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,32 +1,29 @@
 const mongoose = require("mongoose");
 
 const RestaurantSchema = new mongoose.Schema({
-    title: {type: String, require: true},
-    time: {type: String, require: true},
-    value: {type: String, require: true},
-    imageUrl: {type: String, require: true},
-    foods: {type: Array, default:[]},
-    pickup: {type: Boolean, default: true},
-    delivery: {type: Boolean, default: true},
-    isAvailable: {type: Boolean, default: true},
-    owner: {type: String, require: true},
-    code: {type: String, require: true},
-    logoUrl: {type: String, require: true},
-    rating: {type: Number, min:1, max:5, default:3 },
-    ratingCount: {type: String, default: "267"},
-    verification: {type: String, default: "Pending", enum:["Pending", "Verified", "Rejected"]},
-    verification: {type: String, default: "Your Company is under review. We will notify you once it is verified."},
+    title: { type: String, required: true },
+    time: { type: String, required: true },
+    value: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    foods: { type: Array, default: [] },
+    pickup: { type: Boolean, default: true },
+    delivery: { type: Boolean, default: true },
+    isAvailable: { type: Boolean, default: true },
+    owner: { type: String, required: true },
+    code: { type: String, required: true },
+    logoUrl: { type: String, required: true },
+    rating: { type: Number, min:1, max:5, default:3 },
+    ratingCount: { type: String, default: "267" },
+    verification: { type: String, default: "Pending", enum: ["Pending", "Verified", "Rejected"] },
+    verificationMessage: { type: String, default: "Your Company is under review. We will notify you once it is verified." },
     coords: {
-        id: {type: String},
-        latitude: {type: Number, require: true},
-        longitude: {type: Number, require: true},
-        latitudeDelta: {type: Number, default: "0.0122"},
-        longitudeDelta: {type: Number, default: "0.0122"},
-        address: {type: String, require: true},
-        title: {type: String, require: true},
-        
-
+        latitude: { type: Number, required: true },
+        longitude: { type: Number, required: true },
+        latitudeDelta: { type: Number, default: 0.0122 },
+        longitudeDelta: { type: Number, default: 0.0122 },
+        address: { type: String, required: true },
+        title: { type: String, required: true },
     }
 });
 
-module.exports = mongoose.model('Restaurant' , RestaurantSchema);
+module.exports = mongoose.models.Restaurant || mongoose.model('Restaurant' , RestaurantSchema);
