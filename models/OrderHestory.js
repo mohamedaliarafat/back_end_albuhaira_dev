fadd59a1fce09fcf5f63cd155ad9c9ae2c8aa476
mohamedaliarafat <112913473+mohamedaliarafat@users.noreply.github.com>
2025-11-07 22:10:05 +0300

    Update OrderHestory.js

--- models/OrderHestory.js
+++ models/OrderHestory.js
@@ -1,55 +1,55 @@
-const mongoose = require("mongoose");
-const Counter = require("./Counter"); // عداد تلقائي للـ orderId
+// const mongoose = require("mongoose");
+// const Counter = require("./Counter"); // عداد تلقائي للـ orderId
 
-const orderSchema = new mongoose.Schema(
-  {
-    orderId: {
-      type: String,
-      unique: true,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ["Completed", "In-Delivery", "Cancelled"],
-      default: "In-Delivery",
-    },
-    totalAmount: {
-      type: Number,
-      required: true,
-    },
-    currency: {
-      type: String,
-      default: "SAR",
-    },
-    isPast: {
-      type: Boolean,
-      default: false, // الطلب الحالي false، الطلب السابق true
-    },
-  },
-  { timestamps: true }
-);
+// const orderSchema = new mongoose.Schema(
+//   {
+//     orderId: {
+//       type: String,
+//       unique: true,
+//     },
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     status: {
+//       type: String,
+//       enum: ["Completed", "In-Delivery", "Cancelled"],
+//       default: "In-Delivery",
+//     },
+//     totalAmount: {
+//       type: Number,
+//       required: true,
+//     },
+//     currency: {
+//       type: String,
+//       default: "SAR",
+//     },
+//     isPast: {
+//       type: Boolean,
+//       default: false, // الطلب الحالي false، الطلب السابق true
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-// توليد رقم الطلب تلقائي
-orderSchema.pre("save", async function (next) {
-  if (this.isNew && !this.orderId) {
-    try {
-      const counter = await Counter.findOneAndUpdate(
-        { name: "orderId" },
-        { $inc: { seq: 1 } },
-        { new: true, upsert: true }
-      );
-      this.orderId = `ORD${String(counter.seq).padStart(4, "0")}`;
-      next();
-    } catch (err) {
-      next(err);
-    }
-  } else {
-    next();
-  }
-});
+// // توليد رقم الطلب تلقائي
+// orderSchema.pre("save", async function (next) {
+//   if (this.isNew && !this.orderId) {
+//     try {
+//       const counter = await Counter.findOneAndUpdate(
+//         { name: "orderId" },
+//         { $inc: { seq: 1 } },
+//         { new: true, upsert: true }
+//       );
+//       this.orderId = `ORD${String(counter.seq).padStart(4, "0")}`;
+//       next();
+//     } catch (err) {
+//       next(err);
+//     }
+//   } else {
+//     next();
+//   }
+// });
 
-module.exports = mongoose.model("Order", orderSchema);
+// module.exports = mongoose.model("Order", orderSchema);
